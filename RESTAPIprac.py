60b10de6fac0aace5fc684633a998970c999ad7c
pravalcodes <pravalk19@gmail.com>
2022-06-22 17:04:16 -0500

    Test file

--- RESTAPIprac.py
+++ RESTAPIprac.py
@@ -4,7 +4,13 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"Hello World"}
+
+api.add_resource(HelloWorld, "/helloworld")
 if __name__ == "__main__":
     app.run(debug=True)
-    
+
 
