c0e4974fb9611a269ebbfd1fabcfbe1eb2d52d88
pravalcodes <pravalk19@gmail.com>
2022-06-24 22:10:49 -0500

    Simple Changes

--- RESTAPIprac.py
+++ RESTAPIprac.py
@@ -4,12 +4,21 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {"tim" : {"age": 19, "gender": "male"}, 
+        "bill" : {"age":70, "gender" : "male"}}
 
+videos = {}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
 class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
+    def get(self, name):
+        return names[name]
 
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+api.add_resource(Video, "/video/<int:video_id>")
+
 if __name__ == "__main__":
     app.run(debug=True)
 
