c48bb4def91af38bf61a88bdb84eca8fa71f4b21
pravalcodes <pravalk19@gmail.com>
2022-06-28 16:08:36 -0500

    Database changes

--- test.py
+++ test.py
@@ -2,5 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld/bill")
+data = [{"likes":10, "name": "Tim", "views": 10},
+        {"likes":1000, "name": "Elon", "views": 100000},
+        {"likes":1000000, "name": "Praval", "views": 1000000000}]
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
