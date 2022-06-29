d2de89d28cab8cf8bcc37fe96d209b243de69d3c
pravalcodes <pravalk19@gmail.com>
2022-06-29 14:24:24 -0500

    Update Front-End.js

--- Front-End.js
+++ Front-End.js
@@ -8,10 +8,12 @@ submit.onclick = function(){
     var uppercase = document.getElementById("uppercase").value;
 
     if (length <= 0) {
-        var text = "Not a valid length value!";
+        //var text = "Not a valid length value!";
         var new_para = document.createElement("p");
-        var new_text = document.createTextNode(text);
-        new_para.appendChild(new_text);
+        new_para.innerText("Not a valid length value!");
+        //new_para.style.textAlign = center;
+        //var new_text = document.createTextNode(text);
+        //new_para.appendChild(new_text);
         document.body.appendChild(new_para);
     }
 }
