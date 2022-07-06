08f0d18feb2cf131242183d44a4cc9b801b90590
pravalcodes <pravalk19@gmail.com>
2022-07-06 17:02:10 -0500

    Simple Changes

--- Front-End.js
+++ Front-End.js
@@ -1,19 +1,20 @@
-var submit = document.getElementById("submit")
+var submit = document.getElementById("submit");
 
-submit.onclick = function(){
-    var length = document.getElementById("length").value;
+submit.addEventListener("click", )
+
+submit.onclick = function() {
+    var length = document.getElementById("lengcth").value;
     var symbols = document.getElementById("symbols").value;
     var digits = document.getElementById("digits").value;
     var lowercase = document.getElementById("lowercase").value;
     var uppercase = document.getElementById("uppercase").value;
 
     if (length <= 0) {
-        //var text = "Not a valid length value!";
+        var text = "Not a valid length value!";
         var new_para = document.createElement("p");
-        new_para.innerText("Not a valid length value!");
-        //new_para.style.textAlign = center;
-        //var new_text = document.createTextNode(text);
-        //new_para.appendChild(new_text);
+        new_para.style.textAlign = "center";
+        var new_text = document.createTextNode(text);
+        new_para.appendChild(new_text);
         document.body.appendChild(new_para);
     }
-}
+};
